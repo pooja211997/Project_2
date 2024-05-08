324d9b81a7d66c0508852689a334347994e288df
pooja211997 <167012357+pooja211997@users.noreply.github.com>
2024-05-07 22:54:48 -0500

    Project files

---
+++ FirstProject/src/main/java/com/itexps/firstproject/FirstProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexps.firstproject;
+
+/**
+ *
+ * @author phalk
+ */
+public class FirstProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ FirstProject/src/test/java/test/com/EmployeeTest.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package test.com;
+
+import com.itexps.firstproject.Employee;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author phalk
+ */
+public class EmployeeTest {
+    Employee emp;
+    
+    public EmployeeTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        emp= new Employee("tom", 1, "tom@abc.com");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    
+    @Test
+    public void testEmail() throws Exception {
+        assertEquals(emp.getEmail(), "tom@abc.com");
+    }
+    
+    @Test
+    public void testFirst() throws Exception {
+        assertEquals(emp.getFirstname(), "tom");
+    }
+}
